20de7e4756758c5bb277baa2803040395edb1988
Adamb7r <bahrcr7@gmail.com>
2024-04-17 11:40:47 -0700

    fixed

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,9 @@
+vi 0-square
+
 #!/usr/bin/python3
+"""A module for square"""
+
+
 class Square:
+    """Represent a square."""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""Square module."""
+
+
 class Square:
     """Defines a square."""
+
     def __init__(self, size):
-         """Constructor.
+        """Constructor.
         Args:
-            size: length of side.
+            size: length of side of the square.
         """
         self.__size = size
